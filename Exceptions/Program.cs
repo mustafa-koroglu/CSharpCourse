c98f4f21b9bd61f4f11968746f32f2739bd6dbc2
Mustafa Köroğlu <mustafa.koroglu@bilgiedu.net>
2023-03-09 20:47:27 +0300

    Exceptions added.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,73 @@
+﻿using Exceptions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // ExceptionIntro();
+
+        try
+        {
+            Find();
+            
+        } 
+        catch(RecordNotFoundException exception)
+        {
+            Console.WriteLine(exception.Message);
+           
+        }
+
+
+        HandleException(() =>
+        {
+            Find();
+        });
+    }
+
+    private static void HandleException(Action action)
+    {
+        try
+        {
+            action.Invoke();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+            
+
+        }
+    }
+
+    private static void Find()
+    {
+        List<string> students = new List<string> { "Mustafa", "Köroğlu", "Salih" };
+        if (!students.Contains("ahmet"))
+        {
+            throw new RecordNotFoundException("Record not found!");
+            
+        }
+        else
+        {
+            Console.WriteLine("Record Found!");
+            
+        }
+    }
+
+    private static void ExceptionIntro()
+    {
+        try
+        {
+            string[] students = new string[3] { "mustafa", "Köroğlu", "Salih" };
+            students[3] = "Ahmet";
+        }
+        catch (IndexOutOfRangeException exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine(e.Message);
+        }
+    }
+}
